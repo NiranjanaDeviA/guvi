6ac8d13d164c3a0a0dc4ceef0e1567db86b0e3a1
NiranjanaDeviA <51669363+NiranjanaDeviA@users.noreply.github.com>
2019-06-13 11:42:57 +0530

    Update primeinter.py

--- codekata/primeinter.py
+++ codekata/primeinter.py
@@ -1,5 +1,5 @@
-start,end=map(int,input().split()) 
-for val in range(start+1, end-1):
+start,end=map(int,input().split())
+for val in range(start+1, end):
   if val > 1:
     for n in range(2, val):
       if (val % n) == 0:
