69c03622f0e4a59dd9cc08e8f69f409c2dd66ab3
NiranjanaDeviA <51669363+NiranjanaDeviA@users.noreply.github.com>
2019-06-20 10:02:10 +0530

    Create 67mul.py

---
+++ codekata/67mul.py
@@ -0,0 +1,22 @@
+x=int(input())
+y=x
+if x%10==1:
+    print(y+9)
+elif x%10==2:
+    print(y+8)
+elif x%10==3:
+    print(y+7)
+elif x%10==4:
+    print(y+6)
+elif x%10==5:
+    print(y+5)
+elif x%10==6:
+    print(y+4)
+elif x%10==7:
+    print(y+3)
+elif x%10==8:
+    print(y+2)
+elif x%10==9:
+    print(y+1)
+else:
+    print(y)
